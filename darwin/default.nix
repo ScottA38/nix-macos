cf39d58dd42900a3522a539c5b21ba9c7f2dad46
ScottA38 <simple.scott.94@googlemail.com>
2026-01-26 11:00:49 +0000

    main - Reorganise nix-darwin packages

--- darwin/default.nix
+++ darwin/default.nix
@@ -18,6 +18,7 @@
 
   environment.systemPackages = with pkgs; [
     curl
+    openssh
     jq
     glow
     tree
--- darwin/packages.nix
+++ darwin/packages.nix
@@ -2,6 +2,8 @@
 
 {
   environment.systemPackages = with pkgs; [
+    gh
+    glow
     ucto
     chezmoi
     imagemagick
